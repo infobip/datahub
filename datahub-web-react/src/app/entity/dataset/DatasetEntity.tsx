415f1fc6daf7ffde2971c18867d78d167d360022
pavel-istomin <78382785+pavel-istomin@users.noreply.github.com>
2022-09-30 11:48:37 +0200

    Di 1467 add containers entities for parents during ingestion (#22)

--- datahub-web-react/src/app/entity/dataset/DatasetEntity.tsx
+++ datahub-web-react/src/app/entity/dataset/DatasetEntity.tsx
@@ -321,6 +321,7 @@ export class DatasetEntity implements Entity<Dataset> {
                 urn={data.urn}
                 name={data.properties?.qualifiedName || data.properties?.name || data.name}
                 name={data.editableProperties?.name || data.properties?.name || data.name}
+                name={data.properties?.name || data.name}
                 origin={data.origin}
                 description={data.editableProperties?.description || data.properties?.description}
                 platformName={
