db0ed8e3e5bb303b6770b0b9a5f9e7f7ce694ff0
Pavel Istomin <Pavel.Istomin@infobip.com>
2022-10-20 15:31:20 +0200

    f

--- metadata-ingestion/src/datahub/ingestion/source/ib/ib_kafka.py
+++ metadata-ingestion/src/datahub/ingestion/source/ib/ib_kafka.py
@@ -1,10 +1,7 @@
 from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.api.decorators import config_class, platform_name
 from datahub.ingestion.api.ingestion_job_checkpointing_provider_base import JobId
-from datahub.ingestion.source.ib.ib_common import (
-    IBRedashDatasetSource,
-    IBRedashSourceConfig,
-)
+from datahub.ingestion.source.ib.ib_common import *
 
 
 class IBKafkaSourceConfig(IBRedashSourceConfig):
