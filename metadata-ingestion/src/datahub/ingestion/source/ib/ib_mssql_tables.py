fcd021c4be7b04da013324cfc9f3b7828929751e
Pavel Istomin <Pavel.Istomin@infobip.com>
2022-10-21 11:59:53 +0200

    f

--- metadata-ingestion/src/datahub/ingestion/source/ib/ib_mssql_tables.py
+++ metadata-ingestion/src/datahub/ingestion/source/ib/ib_mssql_tables.py
@@ -1,6 +1,11 @@
+from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.api.decorators import config_class, platform_name
-
-from datahub.ingestion.source.ib.ib_common import *
+from datahub.ingestion.source.state.stateful_ingestion_base import JobId
+from datahub.ingestion.source.ib.ib_common import (
+    IBRedashDatasetSource,
+    IBRedashSourceConfig,
+    IBPathElementInfo,
+)
 
 
 class IBMSSQLTablesSourceConfig(IBRedashSourceConfig):
