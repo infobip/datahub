3b170d1a453b560a74ab7f39cc87a4968717190c
Anton Rosanda <Anton.Rosanda@infobip.com>
2024-07-31 17:09:30 +0200

    Revert "delete commented lines 3"
    This reverts commit 1e105c7faf3bc8bd3d1a7ea69b2cec09015d8874.

--- metadata-service/auth-filter/src/main/java/com/datahub/auth/authentication/filter/AuthenticationFilter.java
+++ metadata-service/auth-filter/src/main/java/com/datahub/auth/authentication/filter/AuthenticationFilter.java
@@ -41,6 +41,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
